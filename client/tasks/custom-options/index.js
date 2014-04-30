b4a177fdff585420f3ea4b6074564ff5c6b5ef3b
Ava <avacollins@comcast.net>
2014-04-30 08:12:29 -0700

    adding index task

---
+++ client/tasks/custom-options/index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  /**
+  * During development, we don't want to have wait for compilation,
+  * concatenation, minification, etc. So to avoid these steps, we simply
+  * add all script files directly to the `<head>` of `index.html`. The
+  * `src` property contains the list of included files.
+  */
+	build: {
+		dir: '<%= build_dir %>',
+        src: [
+          '<%= vendor_files.js %>',
+          '<%= build_dir %>/src/**/*.js',
+          '<%= html2js.common.dest %>',
+          '<%= html2js.app.dest %>',
+          '<%= vendor_files.css %>',
+          '<%= build_dir %>/assets/main.css'
+          // '<%= sass.build.dest %>'
+        ]
+	},
+  /**
+  * When it is time to have a completely compiled application, we 
+  * only a single JavaScript and a single CSS file.
+  */
+	compile: {
+		dir: '<%= compile_dir %>',
+        src: [
+          // '<%= concat.compile_js.dest %>',
+          '<%= vendor_files.css %>',
+          '<%= compile_dir %>/assets/main.css'
+          // '<%= recess.compile.dest %>'
+        ]
+	}
+}
