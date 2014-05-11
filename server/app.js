7d532ed3677b872d30de965112ad8482bd747214
Ava <avacollins@comcast.net>
2014-05-10 18:05:38 -0700

    connecting to mongo

--- server/app.js
+++ server/app.js
@@ -10,7 +10,8 @@ var express = require('express'),
 	errorHandler = require('errorHandler'),
 	fs = require('fs'),
 	app = express(),
-	config = require('./config.json') [app.get('env')];
+	config = require('./config.json') [app.get('env')],
+	mongo = require('mongodb');
 
 app.set('port', process.env.PORT || 3000);
 app.use(favicon());
@@ -20,25 +21,46 @@ app.use(logger({
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+// use __dirname make them actually seem like they're coming from the top level
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 var env = process.env.NODE_ENV || 'development';
 if ('development' == env) {
-	app.use(errorHandler())
+	app.use(errorHandler());
+	// use stack trace in development
+	app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
+
 
 app.get('/', routes.index);
 app.get('/api', api.root);
+app.get('/api/cards', api.getAllCards);
+
 
 app.use(function (req,res){
 	res.status(400);
 	res.json({
-		"message": "Resource Not Found"
+		"data": "Resource Not Found"
 	});
 });
 
+app.use(function(err, req, res, next) {
+    res.status(500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
-  console.log(config.db_host)
 });
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,3 +1,17 @@
+var env = process.env.NODE_ENV || 'development',
+	config = require('../config.json') [env],
+	monk = require('monk'),
+	db = monk(config.db_host + '/' + config.db_name),
+	cards = db.get('cards');
+
+
 exports.root = function (req, res){
-	res.json({'message':'This is the root level of the api.'})	
-}
+	res.json({'data':'You must enter a query in the form of api/card/:index/:property?'})	
+};
+
+exports.getAllCards = function (req, res){
+	cards.find({}, function (err, cards) {
+		if(err) res.json(500, err);
+		else res.json(cards);
+	});
+};
